b9347d987535d555eb7971994a5319137fae2e47
Ulisses <iamurise@gmail.com>
2025-01-06 11:55:33 -0300

    Implement SimpleTimeIntervalIntersectionTest properly

--- src/SimpleTimeIntervalIntersectionTest.php
+++ src/SimpleTimeIntervalIntersectionTest.php
@@ -6,52 +6,95 @@ use PHPUnit\Framework\TestCase;
 
 class SimpleTimeIntervalIntersectionTest extends TestCase
 {
-    public function testWithIntersection()
-    {
-        // A        ████
-        // B          ████
-        // A ∪ B    ██████
 
-        $a = new SimpleTimeInterval(
-            new \DateTime('0001-01-01'),
-            new \DateTime('0001-01-03'),
-        );
-        $b = new SimpleTimeInterval(
-            new \DateTime('0001-01-02'),
-            new \DateTime('0001-01-04'),
-        );
-
-        $a_intersection_b = $a->intersection($b);
-
-        $this->assertEquals(
-            $a_intersection_b,
-            new SimpleTimeInterval(
-                new \DateTime('0001-01-02'),
-                new \DateTime('0001-01-03')
-            )
-        );
-    }
-
-    public function testWithoutIntersection()
+    public function testWithABeforeB()
     {
         // A        ██
         // B            ██
-        // A ∪ B    ██  ██
+        // A ∩ B    
 
-        $a = new SimpleTimeInterval(
-            new \DateTime('0001-01-01'),
-            new \DateTime('0001-01-02'),
-        );
-        $b = new SimpleTimeInterval(
-            new \DateTime('0001-01-03'),
-            new \DateTime('0001-01-04'),
-        );
+        [$a, $b] = TestUtil::createABeforeB();
 
         $a_intersection_b = $a->intersection($b);
 
-        $this->assertEquals(
-            $a_intersection_b,
-            new EmptyTimeInterval()
-        );
+        $this->assertEquals($a_intersection_b, new EmptyTimeInterval());
+    }
+
+    public function testWithAAfterB()
+    {
+        // A            ██
+        // B        ██
+        // A ∩ B    
+
+        [$a, $b] = TestUtil::createAAfterB();
+
+        $a_intersection_b = $a->intersection($b);
+
+        $this->assertEquals($a_intersection_b, new EmptyTimeInterval());
+    }
+
+    public function testWithAEngingInB()
+    {
+        // A        ████
+        // B          ████
+        // A ∩ B      ██
+
+        [$a, $b] = TestUtil::createAEndingInB();
+
+        $a_intersection_b = $a->intersection($b);
+
+        $this->assertEquals($a_intersection_b, TestUtil::createSimpleTimeInterval(2, 3));
+    }
+
+    public function testWithAStartingInB()
+    {
+        // A          ████
+        // B        ████
+        // A ∩ B      ██
+
+        [$a, $b] = TestUtil::createAStartingInB();
+
+        $a_intersection_b = $a->intersection($b);
+
+        $this->assertEquals($a_intersection_b, TestUtil::createSimpleTimeInterval(2, 3));
+    }
+
+    public function testWithAContainingB()
+    {
+        // A        ██████
+        // B          ██
+        // A ∩ B      ██
+
+        [$a, $b] = TestUtil::createAContainingB();
+
+        $a_intersection_b = $a->intersection($b);
+
+        $this->assertEquals($a_intersection_b, TestUtil::createSimpleTimeInterval(2, 3));
+    }
+
+    public function testWithAContainedByB()
+    {
+        // A          ██
+        // B        ██████
+        // A ∩ B      ██
+
+        [$a, $b] = TestUtil::createAContainedByB();
+
+        $a_intersection_b = $a->intersection($b);
+
+        $this->assertEquals($a_intersection_b, TestUtil::createSimpleTimeInterval(2, 3));
+    }
+
+    public function testWithAEqualB()
+    {
+        // A        ██████
+        // B        ██████
+        // A ∩ B    ██████
+
+        [$a, $b] = TestUtil::createAEqualB();
+
+        $a_intersection_b = $a->intersection($b);
+
+        $this->assertEquals($a_intersection_b, TestUtil::createSimpleTimeInterval(1, 4));
     }
 }
