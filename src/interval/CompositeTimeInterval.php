18d1332d919dd2bec93865738824c6de75b89c08
Ulisses <iamurise@gmail.com>
2025-01-14 14:18:13 -0300

    Create CompositeTimeInterval union

--- src/interval/CompositeTimeInterval.php
+++ src/interval/CompositeTimeInterval.php
@@ -1,6 +1,9 @@
 <?php
 namespace Yolisses\TimeConstraints\Interval;
 
+
+$counter = 0;
+
 class CompositeTimeInterval implements TimeInterval
 {
     /**
@@ -24,6 +27,27 @@ class CompositeTimeInterval implements TimeInterval
 
     function union(TimeInterval $time_interval): TimeInterval
     {
+        $time_intervals = $this->time_intervals;
+
+        if ($time_interval instanceof CompositeTimeInterval) {
+            $time_intervals = array_merge($time_intervals, $time_interval->getIntervals());
+        } else {
+            $time_intervals[] = $time_interval;
+        }
+
+        if (empty($time_intervals)) {
+            return new EmptyTimeInterval();
+        }
+
+        print_r($time_intervals);
+
+        throw new \Exception('Debugging');
+
+        $result = $time_intervals[0];
+        for ($i = 1; $i < count($time_intervals); $i++) {
+            $result = $result->union($time_intervals[$i]);
+        }
+        return $result;
     }
 
     function intersection(TimeInterval $time_interval): TimeInterval
