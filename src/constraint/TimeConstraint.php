4d80a7e3efdad09a44150150c60d1ae7ae60792a
Ulisses <iamurise@gmail.com>
2025-01-15 11:04:10 -0300

    Add docblock to TimeConstraint::getIntervals

--- src/constraint/TimeConstraint.php
+++ src/constraint/TimeConstraint.php
@@ -7,5 +7,11 @@ use Yolisses\TimeConstraints\Interval\TimeInterval;
 
 abstract class TimeConstraint
 {
-    abstract public function getIntervals(DateTime $start_instant, DateTime $end_instant): TimeInterval;
+    /**
+     * Returns the intervals that satisfy the constraint between the given instants.
+     * @param \DateTime $start_instant
+     * @param \DateTime $end_instant
+     * @return array<TimeInterval>
+     */
+    abstract public function getIntervals(DateTime $start_instant, DateTime $end_instant): array;
 }
