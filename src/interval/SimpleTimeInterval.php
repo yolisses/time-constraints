72fa5b08041c5f54cf50dfe65743abf3ee5ea239
Ulisses <iamurise@gmail.com>
2025-01-15 09:40:06 -0300

    Remove position helper functions from SimpleTimeInterval

--- src/interval/SimpleTimeInterval.php
+++ src/interval/SimpleTimeInterval.php
@@ -9,26 +9,4 @@ class SimpleTimeInterval
             throw new \InvalidArgumentException('Start date must be before end date');
         }
     }
-
-    public function getIsBefore(SimpleTimeInterval $simple_time_interval): bool
-    {
-        return $this->end < $simple_time_interval->start;
-    }
-
-    public function getIsAfter(SimpleTimeInterval $simple_time_interval): bool
-    {
-        return $this->start > $simple_time_interval->end;
-    }
-
-    public function getIsEqual(SimpleTimeInterval $simple_time_interval): bool
-    {
-        return $this->start == $simple_time_interval->start
-            && $this->end == $simple_time_interval->end;
-    }
-
-    public function getIsIntersecting(SimpleTimeInterval $simple_time_interval): bool
-    {
-        return !$this->getIsBefore($simple_time_interval)
-            && !$this->getIsAfter($simple_time_interval);
-    }
 }
