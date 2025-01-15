f1982b6331e4591f10a74c8df32adea92d6329bf
Ulisses <iamurise@gmail.com>
2025-01-15 09:24:29 -0300

    Remove empty and composite time intervals

--- src/interval/TimeInterval.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Yolisses\TimeConstraints\Interval;
-
-interface TimeInterval
-{
-    function union(TimeInterval $time_interval): TimeInterval;
-    function intersection(TimeInterval $time_interval): TimeInterval;
-    function difference(TimeInterval $time_interval): TimeInterval;
-}
