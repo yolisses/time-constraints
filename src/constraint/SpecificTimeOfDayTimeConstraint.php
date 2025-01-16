73f9e354b1459cb4b5875c95c5eb84fd5bf83ca9
Ulisses <iamurise@gmail.com>
2025-01-16 08:32:41 -0300

    Create and test SpecificTimeOfDayTimeConstraint

---
+++ src/constraint/SpecificTimeOfDayTimeConstraint.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Yolisses\TimeConstraints\Constraint;
+
+use Yolisses\TimeConstraints\Constraint\TimeConstraint;
+use Yolisses\TimeConstraints\Interval\TimeInterval;
+
+/**
+ * Time constraint for a specific time of day. E.g. only from 10:00:00 to 12:00:00.
+ */
+class SpecificTimeOfDayTimeConstraint extends TimeConstraint
+{
+    /**
+     * @param \DateTime $time_start e.g. `new DateTime('10:00:00')`
+     * @param \DateTime $time_end e.g. `new DateTime('12:00:00')`
+     */
+    public function __construct(public \DateTime $time_start, public \DateTime $time_end)
+    {
+    }
+
+    static function getCloneWithTime(\DateTime $dateTime, \DateTime $time): \DateTime
+    {
+        $dateTime = clone $dateTime;
+        $dateTime->setTime(
+            $time->format('H'),
+            $time->format('i'),
+            $time->format('s'),
+            $time->format('u'),
+        );
+        return $dateTime;
+    }
+
+    public function getIntervals(\DateTime $start_instant, \DateTime $end_instant): array
+    {
+        $intervals = [];
+
+        $current_instant = self::getCloneWithTime($start_instant, $this->time_start);
+
+        while ($current_instant < $end_instant) {
+            $interval_end = self::getCloneWithTime($current_instant, $this->time_end);
+
+            $intervals[] = new TimeInterval(clone $current_instant, $interval_end);
+
+            $current_instant->modify('+1 day');
+        }
+
+
+        return $this->clampIntervals($intervals, $start_instant, $end_instant);
+    }
+}
---
+++ src/constraint/SpecificTimeOfDayTimeConstraintTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Yolisses\TimeConstraints\Constraint\SpecificTimeOfDayTimeConstraint;
+use Yolisses\TimeConstraints\Interval\TimeInterval;
+
+class SpecificTimeOfDayTimeConstraintTest extends TestCase
+{
+    function testGetIntervals()
+    {
+        $time_start = new DateTime('10:00:00');
+        $time_end = new DateTime('12:00:00');
+        $constraint = new SpecificTimeOfDayTimeConstraint($time_start, $time_end);
+
+        $start_instant = new DateTime('2025-01-01 11:03:04');
+        $end_instant = new DateTime('2025-01-09 11:06:07');
+
+        $intervals = $constraint->getIntervals($start_instant, $end_instant);
+        $this->assertEquals([
+            new TimeInterval(new DateTime('2025-01-01 11:03:04'), new DateTime('2025-01-01 12:00')),
+            new TimeInterval(new DateTime('2025-01-02 10:00'), new DateTime('2025-01-02 12:00')),
+            new TimeInterval(new DateTime('2025-01-03 10:00'), new DateTime('2025-01-03 12:00')),
+            new TimeInterval(new DateTime('2025-01-04 10:00'), new DateTime('2025-01-04 12:00')),
+            new TimeInterval(new DateTime('2025-01-05 10:00'), new DateTime('2025-01-05 12:00')),
+            new TimeInterval(new DateTime('2025-01-06 10:00'), new DateTime('2025-01-06 12:00')),
+            new TimeInterval(new DateTime('2025-01-07 10:00'), new DateTime('2025-01-07 12:00')),
+            new TimeInterval(new DateTime('2025-01-08 10:00'), new DateTime('2025-01-08 12:00')),
+            new TimeInterval(new DateTime('2025-01-09 10:00'), new DateTime('2025-01-09 11:06:07')),
+        ], $intervals);
+    }
+}
