a71bec61c9eb91f9b622d4651adbeade1d5e2e1f
Ulisses <iamurise@gmail.com>
2025-01-27 15:18:17 -0300

    Allow TimeInterval with duration 0

--- src/interval/TimeInterval.php
+++ src/interval/TimeInterval.php
@@ -9,8 +9,8 @@ class TimeInterval
 {
     public function __construct(public \DateTime $start, public \DateTime $end)
     {
-        if (!($end > $start)) {
-            throw new \InvalidArgumentException('End must be greater than start');
+        if (!($end >= $start)) {
+            throw new \InvalidArgumentException('End must be equals or greater than start');
         }
     }
 
