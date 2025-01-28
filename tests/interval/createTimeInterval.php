257fdd90c9b6803c3fc589ec012129ed12ca7c75
Ulisses <iamurise@gmail.com>
2025-01-28 11:48:23 -0300

    Make createTimeInterval more robust

--- tests/interval/createTimeInterval.php
+++ tests/interval/createTimeInterval.php
@@ -4,5 +4,13 @@ use Yolisses\TimeConstraints\Interval\TimeInterval;
 
 function createTimeInterval(int $start, int $end)
 {
-    return TimeInterval::fromStrings("2001-01-$start", "2001-01-$end");
+    $randomly_chosen_number = 41278;
+
+    $scaled_start = $start * $randomly_chosen_number;
+    $start_datetime = (new DateTimeImmutable("2021-01-01 00:00:00"))->modify("$scaled_start seconds");
+
+    $scaled_end = $end * $randomly_chosen_number;
+    $end_datetime = (new DateTimeImmutable("2021-01-01 00:00:00"))->modify("$scaled_end seconds");
+
+    return new TimeInterval($start_datetime, $end_datetime);
 }
