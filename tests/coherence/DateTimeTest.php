0dec26e97dc928aa50dc6180d3c85276c1d43833
Ulisses <iamurise@gmail.com>
2025-01-28 08:27:00 -0300

    Add conclusion in DateTimeTest

--- tests/coherence/DateTimeTest.php
+++ tests/coherence/DateTimeTest.php
@@ -7,6 +7,12 @@ class DateTimeTest extends TestCase
     function testAddSeconds()
     {
         $date = new DateTimeImmutable('2025-01-01 00:00');
+
+        $this->assertEquals(new DateTimeImmutable('2025-01-01 00:00:01'), $date->modify('1 second'));
+        $this->assertEquals(new DateTimeImmutable('2025-01-01 00:01:00'), $date->modify('60 second'));
+        $this->assertEquals(new DateTimeImmutable('2025-01-01 00:00:01'), $date->modify('1 seconds'));
+        $this->assertEquals(new DateTimeImmutable('2025-01-01 00:01:00'), $date->modify('60 seconds'));
+
         $this->assertEquals(new DateTimeImmutable('2025-01-01 00:00:01'), $date->modify('+1 second'));
         $this->assertEquals(new DateTimeImmutable('2025-01-01 00:01:00'), $date->modify('+60 second'));
         $this->assertEquals(new DateTimeImmutable('2025-01-01 00:00:01'), $date->modify('+1 seconds'));
@@ -17,9 +23,12 @@ class DateTimeTest extends TestCase
         $this->assertEquals(new DateTimeImmutable('2024-12-31 23:59:59'), $date->modify('-1 seconds'));
         $this->assertEquals(new DateTimeImmutable('2024-12-31 23:59:00'), $date->modify('-60 seconds'));
 
-        $this->assertEquals(new DateTimeImmutable('2025-01-01 00:00:01'), $date->modify('1 second'));
-        $this->assertEquals(new DateTimeImmutable('2025-01-01 00:01:00'), $date->modify('60 second'));
-        $this->assertEquals(new DateTimeImmutable('2025-01-01 00:00:01'), $date->modify('1 seconds'));
-        $this->assertEquals(new DateTimeImmutable('2025-01-01 00:01:00'), $date->modify('60 seconds'));
+        // Unexpected behavior
+        $this->assertNotEquals(new DateTimeImmutable('2024-12-31 23:59:59'), $date->modify('+-1 second'));
+        $this->assertNotEquals(new DateTimeImmutable('2024-12-31 23:59:00'), $date->modify('+-60 second'));
+        $this->assertNotEquals(new DateTimeImmutable('2024-12-31 23:59:59'), $date->modify('+-1 seconds'));
+        $this->assertNotEquals(new DateTimeImmutable('2024-12-31 23:59:00'), $date->modify('+-60 seconds'));
+
+        // Conclusion: don't use sign in `modify('$someValue seconds')`
     }
 }
