4c438bc870ac80fb4f6fdc43113c56d5c01fe949
Ulisses <iamurise@gmail.com>
2025-01-28 08:56:41 -0300

    Fix SingleDayTimeConstraint

--- src/constraint/SingleDayTimeConstraint.php
+++ src/constraint/SingleDayTimeConstraint.php
@@ -13,7 +13,7 @@ class SingleDayTimeConstraint extends TimeConstraint
     public function getIntervals(\DateTimeImmutable $start_instant, \DateTimeImmutable $end_instant): array
     {
         $start = new \DateTimeImmutable($this->day->format('Y-m-d'));
-        $end = new \DateTimeImmutable($this->day->format('Y-m-d'))->modify('+1 day');
+        $end = (new \DateTimeImmutable($this->day->format('Y-m-d')))->modify('+1 day');
 
         $intervals = [new TimeInterval($start, $end),];
 
