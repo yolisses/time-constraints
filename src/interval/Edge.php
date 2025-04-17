4b95c73a29eb3f5901c4eabcfde52c5842d2229c
Ulisses <iamurise@gmail.com>
2025-04-17 10:31:09 -0300

    Use snake_case in all edge attributes

--- src/interval/Edge.php
+++ src/interval/Edge.php
@@ -7,16 +7,19 @@ class Edge
     public function __construct(
         public \DateTimeImmutable $instant,
         public bool $isStart,
-        public bool $isIncluded
+        public bool $is_included
     ) {
     }
 
+    /**
+     * @param array<TimeInterval> $time_intervals
+     */
     static function getTimeIntervalsEdges(array $time_intervals): array
     {
         $edges = [];
         foreach ($time_intervals as $time_interval) {
-            $edges[] = new Edge($time_interval->start, true, $time_interval->includeStart);
-            $edges[] = new Edge($time_interval->end, false, $time_interval->includeEnd);
+            $edges[] = new Edge($time_interval->start, true, $time_interval->include_start);
+            $edges[] = new Edge($time_interval->end, false, $time_interval->include_end);
         }
 
         return $edges;
