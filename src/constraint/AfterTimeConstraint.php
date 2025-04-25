28f765e44896c154524bd57d1107d0eaad7d8b10
Ulisses <iamurise@gmail.com>
2025-04-25 13:33:15 -0300

    Create AfterTimeConstraint

---
+++ src/constraint/AfterTimeConstraint.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Yolisses\TimeConstraints\Constraint;
+
+use Yolisses\TimeConstraints\Interval\TimeInterval;
+
+class AfterTimeConstraint extends TimeConstraint
+{
+    public function __construct(public \DateTimeImmutable $instant)
+    {
+    }
+
+    public function getIntervals(\DateTimeImmutable $start_instant, \DateTimeImmutable $end_instant): array
+    {
+        //     s   e   
+        //           i
+        //           ██
+        if ($this->instant > $end_instant) {
+            return [];
+        }
+
+        //     s   e   
+        //       i
+        //       ██████
+
+        //     s   e   
+        //   i
+        //   ██████████
+        $intervals = [new TimeInterval($this->instant, $end_instant)];
+        return $this->clampIntervals($intervals, $start_instant, $end_instant);
+    }
+}
---
+++ tests/constraint/AfterTimeConstraintTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Yolisses\TimeConstraints\Constraint\AfterTimeConstraint;
+use Yolisses\TimeConstraints\Interval\TimeInterval;
+
+class AfterTimeConstraintTest extends TestCase
+{
+    public function testAfterTimeConstraintBefore()
+    {
+        $instant = new DateTimeImmutable('2025-01-03');
+        $constraint = new AfterTimeConstraint($instant);
+
+        $start_instant = new DateTimeImmutable('2025-01-01');
+        $end_instant = new DateTimeImmutable('2025-01-02');
+
+        $intervals = $constraint->getIntervals($start_instant, $end_instant);
+        $this->assertEquals([], $intervals);
+    }
+
+    public function testAfterTimeConstraintDuring()
+    {
+        $instant = new DateTimeImmutable('2025-01-03');
+        $constraint = new AfterTimeConstraint($instant);
+
+        $start_instant = new DateTimeImmutable('2025-01-02');
+        $end_instant = new DateTimeImmutable('2025-01-04');
+
+        $intervals = $constraint->getIntervals($start_instant, $end_instant);
+        $this->assertEquals([new TimeInterval($instant, $end_instant)], $intervals);
+    }
+
+    public function testAfterTimeConstraintAfter()
+    {
+        $instant = new DateTimeImmutable('2025-01-03');
+        $constraint = new AfterTimeConstraint($instant);
+
+        $start_instant = new DateTimeImmutable('2025-01-04');
+        $end_instant = new DateTimeImmutable('2025-01-05');
+
+        $intervals = $constraint->getIntervals($start_instant, $end_instant);
+        $this->assertEquals([new TimeInterval($start_instant, $end_instant)], $intervals);
+    }
+}
